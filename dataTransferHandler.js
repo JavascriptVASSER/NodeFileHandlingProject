25a653fe450006a65d2d5b3a37141b16434c6c81
JavascriptVASSER <104643921+JavascriptVASSER@users.noreply.github.com>
2023-10-21 12:52:06 +0300

    Update dataTransferHandler.js
    Changes Include:
    
     - Asking user if user wants to append until user says 'no'.

--- dataTransferHandler.js
+++ dataTransferHandler.js
@@ -1,9 +1,10 @@
+const { read } = require('fs');
 const fileOperations = require('./fileOperations.js');
 const readline = require('readline');
 
-const dataTransfer = (userInput) => {
-  const [dfile, tfile] = userInput.split(':'); // Split the input into source and target files
-  return fileOperations.Append(tfile, dfile);
+const dataTransfer = (i) => {
+  const [dfile, tfile] = i.split(':'); // Split the input into source and target files
+  return fileOperations.Append(tfile, dfile); // return the result with the imported function: "Append"
 };
 
 const readLineInterface = readline.createInterface({
@@ -11,14 +12,26 @@ const readLineInterface = readline.createInterface({
   output: process.stdout,
 });
 
-readLineInterface.question('Please specify the source (first) and target file (second) for data transfer (separated by a colon: ":"): ', (userInput) => {
-  dataTransfer(userInput)
-    .then(result => {
-      console.log(result);
+const askToAppend = async (qa) => {
+  readLineInterface.question(qa, (input) => {
+    if (input === 'yes'){
+        readLineInterface.question('Enter data file path (1) and target file path (2) separated by a colon: ', (userInput) => {
+          dataTransfer(userInput)
+          .then(result => {
+            console.log(`${result}`)
+            askToAppend('Would you like to append a text file again? ');
+          })
+          .catch(error => {
+            console.log(`THERE WAS AN ERROR ${error}`)
+            askToAppend('Would you like to append a text file again? ');
+          });
+        })
+    }
+    else if (input === `no`){
+      console.log('Thank you for using FTS. Come back soon!')
       readLineInterface.close();
-    })
-    .catch(error => {
-      console.error(error);
-      readLineInterface.close();
-    });
-});
+    }
+  });
+}
+
+askToAppend('Would you like to append a text file file? ');
